7e25480525532ccf618a34b368f82af8fdfa45ac
parth patel <patel2pa@dukes.jmu.edu>
2019-11-03 22:46:55 -0500

    pin and image update

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## Clean code
 
-## prototype 2
 ## live demo at https://ami2.netlify.com
 implemented using mapbox gl js lib, with basic user interface 
-github https://github.com/patel2pa37/aerial-multispectral-app2.git
+
--- src/Components/BaseMap/BaseMap.js
+++ src/Components/BaseMap/BaseMap.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import DeckGL, {BitmapLayer} from 'deck.gl';
-import MapGL,{Marker, GeolocateControl} from 'react-map-gl';
+import MapGL,{Marker, GeolocateControl,Popup} from 'react-map-gl';
 import Pin from './pin.js'
 import SideDrawer from '../SideDrawer/SideDrawer'
 import './BaseMapStyle.css'
+import RGBA from '../Images/rgba.png'
+import MERGED from '../Images/merged.png'
 
 
 const TOKEN = 'pk.eyJ1IjoicGF0ZWwycGEiLCJhIjoiY2sxMnkyczM0MDNxOTNiczluMnRyY2tsMiJ9.0maYtnNj3fQVEJ2BLfvJXA'; // Set your mapbox token here
 const MapStyle = {
-    mapboxDefault:'mapbox://styles/patel2pa/ck12yhywb0jyo1cn7xavx8lik',
+    mapboxDefault:'mapbox://styles/patel2pa/ck1lhtf917qtt1crviwyg2973',
     openMapTile: 'https://api.maptiler.com/maps/hybrid/style.json?key=gQCnC8ZYWGvM8WdKZNmW'
   }
 
@@ -18,6 +20,13 @@ const MapStyle = {
     right: 0,
     margin: 10,   
   };
+
+  const DefaultOverlayBounds = [
+    [-78.4989250540139,37.9307066927,-78.4950953896, 37.933022282],
+    [-78.4977459718528, 37.92754239722171,-78.49195042016456, 37.9330963517]
+  ]
+
+  const DefaultOverlays = [RGBA,MERGED]
 
   
 export default class Test2 extends Component {
@@ -30,7 +39,11 @@ export default class Test2 extends Component {
       width:'',
       height:'',
       data:[],
-      markerData:[]
+      markerData:[],
+      popupInfo:null,
+      delete:false,
+      add: false,
+      inputValue: []
     }
 
   componentDidMount() {
@@ -39,7 +52,7 @@ export default class Test2 extends Component {
   }
 
   updateWindowDimensions = () => {
-    this.setState({ height: window.innerHeight+'px',width:window.innerWidth+'px' });
+    this.setState({ height: window.innerHeight+-40+'px',width:window.innerWidth+'px' });
   }
   
   _onViewportChange = viewport => {
@@ -75,20 +88,97 @@ export default class Test2 extends Component {
   }
 
   _onClickMethod = (map,e) => {
-    var joined = this.state.markerData.concat([e.lngLat]);
-    this.setState({ markerData: joined })
+    if (this.state.add){
+      var joined = this.state.markerData.concat([e.lngLat]);
+      this.setState({ markerData: joined })
+    }
   }
   
   renderMarkersandPin = () => {
-    let markerArray = []
-    if(undefined !== this.state.markerData && this.state.markerData.length){
-      for(var i = 0;i<this.state.markerData.length;i++){
-      markerArray.push(<Marker latitude={this.state.markerData[i][1]} longitude={this.state.markerData[i][0]}  >
-      <Pin size={20} onClick = {(e)=>console.log(e)}/>
-      </Marker>)
-      }
+    const markerData_ = this.state.markerData
+    return this.state.markerData.map((child, index)=>
+    <Marker latitude={this.state.markerData[index][1]} longitude={this.state.markerData[index][0]} >
+      <Pin size={20} key={index} onClick={(e)=>this.DeleteMarkerAndRenderPopupInput(e,index)} />
+    </Marker>)
+}
+
+  DeleteMarkerAndRenderPopupInput(e, i){
+    if(this.state.delete){
+      this.setState({delete:false})
+      this.setState(state => {
+        const markerData = state.markerData.filter((item, j) => i !== j);
+        return {
+        markerData,
+        };
+      })
     }
-    return markerArray
+    else{
+      this.setState({popupInfo:[this.state.markerData[i],i]})
+    }
+  };
+
+  RenderPopups = () => {
+    const {popupInfo} = this.state;
+    return (
+      popupInfo && (<Popup
+        tipSize={8}
+        anchor="bottom"
+        longitude={this.state.popupInfo[0][0]}
+        latitude={ this.state.popupInfo[0][1]}
+        closeOnClick={false}
+        onClose={() => this.setState({popupInfo: null})}
+      >
+        <input value={this.state.inputValue[this.state.popupInfo[1]]} onChange={evt => this.UpdateInputValue(evt, this.state.popupInfo[1])}/>
+      </Popup>
+      )
+    )
+  }
+
+  UpdateInputValue = (evt,index) =>{
+    const updatedArray = [...this.state.inputValue];
+    updatedArray[index] = evt.target.value;
+    this.setState({
+      inputValue: updatedArray,
+      });
+/*
+  this.setState({
+    inputValue: evt.target.value
+  });*/
+  }
+
+  _renderButtonsTools = () => {
+    // copy from mapbox
+    return (
+      <div className='Button1'>
+        <div >
+          <button
+            title="Polygon tool (p)"
+            onClick = {()=>this.setState({add:true})}
+          >Add pins</button>
+          <button
+            className="Button"
+            title="Delete"
+            onClick = {()=>this.setState({add:false,delete:true})}
+          >Delete pins</button>
+        </div>
+      </div>
+    );
+  };
+
+  renderDefaultOverlay = () =>{
+    for(let i = 0; i<DefaultOverlays.length;i++)
+    return (<DeckGL
+      viewState={this.state.viewport}
+      layers={[
+        new BitmapLayer({
+            id: 'bitmap-layer',
+            bounds: DefaultOverlayBounds[i],
+            image: DefaultOverlays[i],
+            transparentColor: [0,0,0,0]
+          })
+      ]}
+    />
+    )
   }
 
   render() {
@@ -110,13 +200,16 @@ export default class Test2 extends Component {
           mapboxApiAccessToken={TOKEN}
           onClick = {(e)=>this._onClickMethod(MapGL,e)}
         >
-          {this.renderMarkersandPin()}
+          {this.renderDefaultOverlay()}
           {this.renderOverlayImages()}
+          {this.renderMarkersandPin()}
+          {this.RenderPopups()}
           <GeolocateControl
               style={geolocateStyle}
               positionOptions={{enableHighAccuracy: false}}
               trackUserLocation = {true}
           />
+          {this._renderButtonsTools()}
         </MapGL>
       </div>
     );
